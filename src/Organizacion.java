e19e982b288a48d87da6d3eeb193404cd3e00201
Max_xie <Max_xie@DESKTOP-391LNV8>
2025-05-28 14:23:48 -0300

    creadas las clases básicas

--- src/Organizacion.java
+++ src/Organizacion.java
@@ -1,4 +1,34 @@
 
 public class Organizacion {
 
+	private Ubicacion ubicacion;
+	private TipoDeOrganizacion tipo;
+	private int cantidadPersonas;
+	
+	public Ubicacion getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(Ubicacion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+	
+	public TipoDeOrganizacion getTipo() {
+		return tipo;
+	}
+	
+	public TipoDeOrganizacion SetTipo(TipoDeOrganizacion tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int getCantidadPersonas() {
+		return cantidadPersonas;
+	}
+	
+	
+	public void setCantidadPersonas(int cantidadPersonas) {
+		this.cantidadPersonas = cantidadPersonas;
+	}
+	
+	
 }
