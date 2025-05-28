4962cc9d348ee6afd776b3f63d277f2b96db14f4
Max_xie <Max_xie@DESKTOP-391LNV8>
2025-05-28 16:51:42 -0300

    actualizado ubicacion

--- src/Ubicacion.java
+++ src/Ubicacion.java
@@ -1,5 +1,24 @@
 
 public class Ubicacion {
-  private Coordenada coordenada;
+  private double latitud;
+  private double longitud;
   
+  public Ubicacion(double latitud, double longitud) {
+	  this.latitud = latitud;
+	  this.longitud = longitud;
+  }
+  
+public double getLatitud() {
+	return latitud;
 }
+public void setLatitud(double latitud) {
+	this.latitud = latitud;
+}
+public double getLongitud() {
+	return longitud;
+}
+public void setLongitud(double longitud) {
+	this.longitud = longitud;
+}
+}
+
