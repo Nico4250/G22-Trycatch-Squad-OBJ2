bd417cfb7b118bb40b1c48c64ec06dd1e9511eb6
Max_xie <Max_xie@DESKTOP-391LNV8>
2025-06-14 18:20:36 -0300

    creado el paquete test

---
+++ test/ar/edu/una/po2/Usuario/UsuarioTest.java
@@ -0,0 +1,14 @@
+package ar.edu.una.po2.Usuario;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UsuarioTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
