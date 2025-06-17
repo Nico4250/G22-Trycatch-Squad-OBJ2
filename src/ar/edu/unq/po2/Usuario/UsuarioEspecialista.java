1187cbe0272c85c32549cc6b0fd94292773da6c9
Max_xie <Max_xie@DESKTOP-391LNV8>
2025-06-17 14:32:33 -0300

    Actualizado Usuario Especialista

--- src/ar/edu/unq/po2/Usuario/UsuarioEspecialista.java
+++ src/ar/edu/unq/po2/Usuario/UsuarioEspecialista.java
@@ -1,8 +1,8 @@
 package ar.edu.unq.po2.Usuario;
 
-public class usuarioEspecialista extends Usuario {
+public class UsuarioEspecialista extends Usuario {
 
-	public usuarioEspecialista(int id, String nombreUsuario, Boolean True) {
+	public UsuarioEspecialista(int id, String nombreUsuario, Boolean True) {
 		super(id, nombreUsuario);
 	//TODAVIA FALTA ALGO QUE COMPRUEBE QUE ES ESPECIALISTA
 	}
